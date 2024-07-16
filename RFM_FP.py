4817f2d64f500397cbac758809987b7bf85bd60b
Yifan Wang <8groupoid@gmail.com>
2024-07-16 16:02:18 -0400

    Added basic layout for fokker-planck RFM

--- RFM_FP.py
+++ RFM_FP.py
@@ -1,3 +1,10 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import math
+from scipy.linalg import lstsq,pinv
+import matplotlib.pyplot as plt
+import random
 
 """
 Settings:
@@ -8,21 +15,15 @@ Bounded potential: V(x) = sin(2\pi x) + cos(4\pi x)
 Hamiltonian: H(x, Du) = 1/2 |Du|^2 - V(x)
 F(m)=m^2
 """
-import numpy as np
+
 
 
 def V(x):
     """
     Analytical bounded potential function
-    :param x:
-    :return:
     """
     return np.sin(2 * np.pi * x) + np.cos(4 * np.pi * x)
 
-def policy_iteration(n_iter=25):
-
-    # initialize policy q
-    for _ in range(n_iter):
 
 
 
