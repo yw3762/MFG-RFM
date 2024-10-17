9717fc6dc6058e29acef2d90ca16ac612a00b161
Yifan Wang <8groupoid@gmail.com>
2024-10-17 17:48:18 -0400

    Added regularization and normalization constraints in inverse MFG

--- rfm_mfg_stationary/inverse_mfg_1d/inverse_policy_iteration.py
+++ rfm_mfg_stationary/inverse_mfg_1d/inverse_policy_iteration.py
@@ -1,18 +1,17 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.linalg import lstsq, pinv
+from scipy.linalg import lstsq, pinv, inv
 from torch import sin, cos, pi, exp
 
 from rfm_mfg_stationary.mfg_1d.policy_iter_1d import solve_FP_forward, policy_iteration, solve_HJB_forward
 from rfm_mfg_stationary.mfg_1d.utils.utils_1d import init_rfm, RFM_function_factory, second_diff_RFM_function, \
-    plot_RFM_1d, lagrangian_1d, plot_RFM_vs_true
+    plot_RFM_1d, lagrangian_1d, plot_RFM_vs_true, set_seed
 from rfm_mfg_stationary.inverse_mfg_1d.utils_inv_1d import RF_valuation, concatenate_collocs, \
     func_valuation, build_RFM_matrix
 
 
-def inverse_HJB(models_u, models_b, collocs_b_cat, m_func, q_func, observed_u, Fl_pinv, H_pinv, U_l, Mu, Mb, Ju, Jb, Qb,
-                eps):
+def inverse_HJB(models_u, models_b, collocs_b_cat, m_func, q_func, observed_u, F_l, Fb_avg, H_b, U_l, Mb, Ju, Jb, Qb,
+                eps, regularization=False):
     """
     Solve the inverse problem for HJB, the observed data g(x) is the observed solution for u(x), which we evaluate
     on finite grid.
@@ -31,15 +30,34 @@ def inverse_HJB(models_u, models_b, collocs_b_cat, m_func, q_func, observed_u, F
 
     W_b = (m_func(collocs_b_cat) ** 2 + lagrangian_1d(collocs_b_cat, q_vals, None)).view(-1, 1).detach().numpy()
 
+    # M is of shape (Qb * Mb, Ju * Mu)
     M = build_RFM_matrix(models_u, q_vals, collocs_b_cat, Mb, Qb, Ju, eps)
-    tilde_theta_nj = torch.tensor((H_pinv @ (M @ Fl_pinv @ U_l - W_b)).T)
-    theta_nj, recovered_lambda = tilde_theta_nj[0,:-1].reshape((Mb, Jb)), tilde_theta_nj[0, -1]
+    Mu = len(models_u)
+    MTM_inv_f = inv(M.T @ M) @ Fb_avg
+    v = MTM_inv_f / (1 + Fb_avg.T @ MTM_inv_f)
+
+    # We modify the test slightly
+    no_normalization_ = False
+    if no_normalization_:
+        Q_lb = F_l @ np.eye(Mu * Ju) @ pinv(M)
+    else:
+        I_bplus = np.eye(Mu * Ju) - np.outer(v, Fb_avg.T)
+        Q_lb = F_l @ I_bplus @ pinv(M)
+
+    if regularization:
+        X = Q_lb @ H_b
+        regularization_constant = 0.001
+        tilde_theta_nj = torch.tensor(inv(X.T @ X + regularization_constant * np.eye(H_b.shape[1])) @ X.T @ (U_l - Q_lb @ W_b)).T
+    else:
+        tilde_theta_nj = torch.tensor(pinv(Q_lb @ H_b) @ (U_l - Q_lb @ W_b)).T
+
+    theta_nj, recovered_lambda = tilde_theta_nj[0, :-1].reshape((Mb, Jb)), tilde_theta_nj[0, -1]
 
     recovered_b = RFM_function_factory(models_b, theta_nj)
     return recovered_b, recovered_lambda
 
 
-def PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb, n_iters=20, eps=0.3, tau=1e-6, plot=False):
+def inverse_PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb, n_iters=20, eps=0.3, tau=1e-6, plot=False):
     """
     Solves the inverse problem with final-time derivative information (i.e. case ii in Ren et al.)
 
@@ -72,12 +90,13 @@ def PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb, n_iters=20, ep
     x_l = torch.linspace(0, 1, steps=Il + 1)[:-1].view(-1, 1)
 
     # Pre-compute F_l, H_b, W_l, U_l matrices (as tensors)
+    # collocs_u_cat = concatenate_collocs(collocs_u)
     collocs_b_cat = concatenate_collocs(collocs_b)
-    F_l = RF_valuation(models_u, x_l)
-    H_b = RF_valuation(models_b, collocs_b_cat)
-    H_b = torch.cat([RF_valuation(models_b, collocs_b_cat), -torch.ones((H_b.shape[0], 1))], dim=1)
-    Fl_pinv = pinv(F_l.detach().numpy())  # F_l^\dagger
-    H_pinv = pinv(H_b.detach().numpy())  # \tilde{H}_b^\dagger
+    F_l = RF_valuation(models_u, x_l).detach().numpy()
+    F_b = RF_valuation(models_u, collocs_b_cat).detach().numpy()
+    Fb_avg = np.mean(F_b, axis=0)
+    H_b = RF_valuation(models_b, collocs_b_cat).detach().numpy()
+    H_b = np.concatenate([H_b, -np.ones((H_b.shape[0], 1))], axis=1)
     U_l = func_valuation(u_true, x_l).detach().numpy()  # U_l
 
     # historical solutions and policies
@@ -97,15 +116,17 @@ def PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb, n_iters=20, ep
 
         # Step (2): Solve Inverse HJB equation for b and lambda
         recovered_b, recovered_lambda = inverse_HJB(models_u, models_b, collocs_b_cat, historical_m[k],
-                                                    historical_q[k - 1], u_true, Fl_pinv, H_pinv, U_l, Mu, Mb, Ju, Jb,
-                                                    Qb, eps)
+                                                    historical_q[k - 1], u_true, F_l, Fb_avg, H_b, U_l, Mb, Ju, Jb, Qb,
+                                                    eps, regularization=True)
         historical_b.append(recovered_b)
         historical_lam.append(recovered_lambda)
 
         # Step (3): Obtain u from recovered b, and update policy from the newly obtained u.
-        curr_u, curr_lam = solve_HJB_forward(models_u, collocs_u, historical_m[k], historical_q[k-1], Mu, Ju, Qu,
+        curr_u, curr_lam = solve_HJB_forward(models_u, collocs_u, historical_m[k], historical_q[k - 1], Mu, Ju, Qu,
                                              lagrangian_1d, b=recovered_b, eps=eps)
         historical_u.append(curr_u)
+
+        print("Lambda gap is: " + str(curr_lam[0] - recovered_lambda.item()))
 
         new_q, dq = second_diff_RFM_function(historical_u[k])
         historical_q.append(new_q)
@@ -114,6 +135,7 @@ def PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb, n_iters=20, ep
 
 
 def test():
+    set_seed(100)
     Mu, Mm, Mb = 4, 4, 5
     Ju, Jm, Jb = 20, 20, 30
     Il = 400
@@ -122,11 +144,11 @@ def test():
     def true_b(x):
         return 0.1 * (sin(2 * pi * x - sin(4 * pi * x)) + exp(cos(2 * pi * x)))
 
-    m_true, u_true, _ = policy_iteration(Mu, Ju, Mm, Jm, Qu, Qm, b=true_b, n_iters=4)
-    recovered_b, recovered_lam = PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb)
+    m_true, u_true, _ = policy_iteration(Mu, Ju, Mm, Jm, Qu, Qm, b=true_b, n_iters=20)
+    recovered_b, recovered_lam = inverse_PI_stationary(u_true, Mu, Mm, Mb, Ju, Jm, Jb, Il, Qu, Qm, Qb)
 
     # Plot the recovered b against true b
-    plot_RFM_vs_true(recovered_b, true_b, recovered_lam,"b")
+    plot_RFM_vs_true(recovered_b, true_b, recovered_lam, "b")
 
 
 test()
